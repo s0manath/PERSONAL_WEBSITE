896d78a0b8d19da47a1b625b5fe5407280b2bfcb
s0manath <soma756126@gmail.com>
2024-04-15 19:19:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
