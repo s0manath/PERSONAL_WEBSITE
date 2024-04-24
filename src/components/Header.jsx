6b0a5ed8fe15e8990c789f8cd982f26fafc82521
s0manath <soma756126@gmail.com>
2024-04-24 19:11:31 +0530

    pdf implimentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import myPdf from '../assets/somanath.pdf'
 
 const Header = () => {
 
@@ -36,7 +37,7 @@ const Header = () => {
 });
     const handleDownloadPDF = () => {
     // Replace 'your-pdf-file.pdf' with the actual path to your PDF file
-    window.open('../assets/somanath.pdf', '_blank');
+    window.open(myPdf, '_blank');
     };
 
 
